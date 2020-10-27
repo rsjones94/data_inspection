a219963189024f4a4c173ef631df51b4da5b2a96
Sky Jones <rsajones94@gmail.com>
2020-10-27 11:52:20 -0500

    no message

--- data_inspection/visualize_freesurfer_volumes.py
+++ data_inspection/visualize_freesurfer_volumes.py
@@ -20,6 +20,7 @@ from scipy import stats
 from sklearn.ensemble import IsolationForest
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn import preprocessing
+import scipy
 
 from matplotlib.patches import Circle
 from matplotlib.offsetbox import (TextArea, DrawingArea, OffsetImage,
@@ -46,6 +47,66 @@ visualize = True
 # os.path.basename(os.path.normpath(path))
 
 ###########
+
+
+def plot_ci_manual(t, s_err, n, x, x2, y2, ax=None):
+    """Return an axes of confidence bands using a simple approach.
+
+    Notes
+    -----
+    .. math:: \left| \: \hat{\mu}_{y|x0} - \mu_{y|x0} \: \right| \; \leq \; T_{n-2}^{.975} \; \hat{\sigma} \; \sqrt{\frac{1}{n}+\frac{(x_0-\bar{x})^2}{\sum_{i=1}^n{(x_i-\bar{x})^2}}}
+    .. math:: \hat{\sigma} = \sqrt{\sum_{i=1}^n{\frac{(y_i-\hat{y})^2}{n-2}}}
+
+    References
+    ----------
+    .. [1] M. Duarte.  "Curve fitting," Jupyter Notebook.
+       http://nbviewer.ipython.org/github/demotu/BMC/blob/master/notebooks/CurveFitting.ipynb
+
+    """
+    if ax is None:
+        ax = plt.gca()
+
+    ci = t * s_err * np.sqrt(1/n + (x2 - np.mean(x))**2 / np.sum((x - np.mean(x))**2))
+    ax.fill_between(x2, y2 + ci, y2 - ci, color="#b9cfe7", edgecolor="")
+
+    return ax
+
+
+def plot_ci_bootstrap(xs, ys, resid, nboot=500, ax=None):
+    """Return an axes of confidence bands using a bootstrap approach.
+
+    Notes
+    -----
+    The bootstrap approach iteratively resampling residuals.
+    It plots `nboot` number of straight lines and outlines the shape of a band.
+    The density of overlapping lines indicates improved confidence.
+
+    Returns
+    -------
+    ax : axes
+        - Cluster of lines
+        - Upper and Lower bounds (high and low) (optional)  Note: sensitive to outliers
+
+    References
+    ----------
+    .. [1] J. Stults. "Visualizing Confidence Intervals", Various Consequences.
+       http://www.variousconsequences.com/2010/02/visualizing-confidence-intervals.html
+
+    """
+    if ax is None:
+        ax = plt.gca()
+
+    bootindex = scipy.random.randint
+
+    for _ in range(nboot):
+        resamp_resid = resid[bootindex(0, len(resid) - 1, len(resid))]
+        # Make coeffs of for polys
+        pc = scipy.polyfit(xs, ys + resamp_resid, 1)                  
+        # Plot bootstrap cluster
+        ax.plot(xs, scipy.polyval(pc, xs), "b-", linewidth=2, alpha=3.0 / float(nboot))
+
+    return ax
+
 
 def filter_zeroed_axial_slices(nii_data, thresh=0.99):
     # removes slices if the number of pixels that are lesser than or equal to 0 exceeds a % threshold, and replaces NaN with -1
@@ -110,6 +171,13 @@ if collate:
                   'total_vol':None,
                   'total_vol_custom':None,
                   'mask_vol_custom':None,
+                  'supratent':None,
+                  'etiv':None,
+                  'csv':None,
+                  'gm_normal':None,
+                  'wm_normal':None,
+                  'total_normal':None,
+                  'supratent_normal':None,
                   'age':None,
                   'stroke_silent':None,
                   'stroke_overt':None,
@@ -133,6 +201,13 @@ if collate:
         working['total_vol'] = parsed_csv.loc['BrainSegVolNotVent']['value'] / 1e3
         working['wm_vol'] = working['total_vol'] - working['gm_vol']
         working['age'] = in_table_indexed.loc[pt_name]['age']
+        working['etiv'] = parsed_csv.loc['eTIV']['value'] / 1e3
+        working['gm_normal'] = working['gm_vol'] / working['etiv']
+        working['wm_normal'] = working['wm_vol'] / working['etiv']
+        working['total_normal'] = working['total_vol'] / working['etiv']
+        working['supratent'] = parsed_csv.loc['SupraTentorialVolNotVent']['value'] / 1e3
+        working['supratent_normal'] = working['supratent'] / working['etiv']
+        working['csv'] = working['etiv'] - working['total_vol']
         
         stroke_overt = in_table_indexed.loc[pt_name]['mh_rf_prior_stroke_overt']
         stroke_silent = in_table_indexed.loc[pt_name]['mh_rf_prior_stroke_silent']
@@ -246,14 +321,17 @@ if visualize:
         patch.set_facecolor(color)
     plt.savefig(box_out)
     
-    y_names = ['wm_vol', 'gm_vol', 'total_vol', 'total_vol_custom', 'mask_vol_custom']
+    y_names = ['wm_vol', 'gm_vol', 'total_vol', 'total_vol_custom', 'mask_vol_custom', 'gm_normal', 'wm_normal', 'total_normal', 'etiv', 'supratent', 'supratent_normal']
 
     xlims = [0, 50]
     ylims = [0, 1800]
     
-    genders = [[1,'male'], [0,'female']]
+    genders = [[1,'male'], [0,'female'], [None,'all']]
     for g, g_name in genders:
-        gender_table = clean_table[clean_table['gender'] == g]
+        if g is not None:
+            gender_table = clean_table[clean_table['gender'] == g]
+        else:
+            gender_table = clean_table
         for y_name in y_names:
             fig, ax = plt.subplots(figsize=(12,6))
             plot_name = os.path.join(out_folder, f'{y_name}_{g_name}.png')
@@ -280,9 +358,12 @@ if visualize:
                 plt.plot(lin_ex, lin_y, color=c, alpha = 0.7)
                 
             plt.xlabel('Patient age (years)')
-            plt.ylabel('Volume (cc)')
+            if 'normal' not in y_name:
+                plt.ylabel('Volume (cc)')
+            else:
+                plt.ylabel('Normalized volume (tissue vol / eTIV)')
                    
-            if y_name in ['total_vol', 'total_vol_custom', 'mask_vol_custom']:
+            if y_name in ['total_vol', 'total_vol_custom', 'mask_vol_custom'] and g_name != 'all':
                 plt.plot(brain_vol_df['age'], brain_vol_df[f'fit_{g_name}'], color='purple', alpha=0.25)
                 plt.plot(brain_vol_df['age'], brain_vol_df[f'min_{g_name}'], color='purple', ls='dashed', alpha=0.25)
                 plt.plot(brain_vol_df['age'], brain_vol_df[f'max_{g_name}'], color='purple', ls='dashed', alpha=0.25)
@@ -342,14 +423,125 @@ if visualize:
             
             
             plt.title(f'{y_name} ({g_name})\noutliers: {all_out}')
+            if 'normal' not in y_name:
+                plt.ylim(ylims)
+            else:
+                plt.ylim((0,1))
+                
             plt.xlim(xlims)
-            plt.ylim(ylims)
             
             plt.legend(artists, artist_descs)
             
             plt.tight_layout()
             plt.savefig(plot_name)
-            
         
+    #interest = ['total_vol', 'wm_vol', 'gm_vol', 'csv']
+    interest = ['wm_vol', 'gm_vol', 'csv']
+    pt_types = ['control', 'sci']
+    cols = ['red', 'blue']
+    fig, axs = plt.subplots(len(pt_types), len(interest), figsize=(6*len(interest),4*len(pt_types)))
+    for pt_type, color, axrow in zip(pt_types, cols, axs):
+        expr = clean_table[pt_type] == 1
+        subdf = clean_table[expr]
+        for col, ax in zip(interest, axrow):
+            exes = subdf['age']
+            whys = subdf[col]
+            
+            
+            ## BOOT STRAPPING. courtesu pylang from stackoverflow
+            
+            x, y = exes, whys
+            
+            # Modeling with Numpy
+            def equation(a, b):
+                """Return a 1D polynomial."""
+                return np.polyval(a, b)
+            
+            p, cov = np.polyfit(x, y, 1, cov=True)                     # parameters and covariance from of the fit of 1-D polynom.
+            y_model = equation(p, x)                                   # model using the fit parameters; NOTE: parameters here are coefficients
+            
+            # Statistics
+            n = len(exes)                                         # number of observations
+            m = p.size                                                 # number of parameters
+            dof = n - m                                                # degrees of freedom
+            t = stats.t.ppf(0.975, n - m)                              # used for CI and PI bands
+            
+            # Estimates of Error in Data/Model
+            resid = y - y_model                          
+            chi2 = np.sum((resid / y_model)**2)                        # chi-squared; estimates error in data
+            chi2_red = chi2 / dof                                      # reduced chi-squared; measures goodness of fit
+            s_err = np.sqrt(np.sum(resid**2) / dof)                    # standard deviation of the error
+            
+            # Data
+            ax.plot(
+                x, y, "o", color="#b9cfe7", markersize=8,
+                markeredgewidth=1, markeredgecolor="b", markerfacecolor="None"
+            )
+            
+            # Fit
+            ax.plot(x, y_model, "-", color="0.1", linewidth=1.5, alpha=0.5, label="Fit")  
+            
+            x2 = np.linspace(np.min(x), np.max(x), 100)
+            y2 = equation(p, x2)
+            
+            # Confidence Interval (select one)
+            plot_ci_manual(t, s_err, n, x, x2, y2, ax=ax)
+            #plot_ci_bootstrap(x, y, resid, ax=ax)
+            
+            # Prediction Interval
+            pi = t * s_err * np.sqrt(1 + 1/n + (x2 - np.mean(x))**2 / np.sum((x - np.mean(x))**2))  
+            ax.fill_between(x2, y2 + pi, y2 - pi, color="None", linestyle="--")
+            ax.plot(x2, y2 - pi, "--", color="0.5", label="95% Prediction Limits")
+            ax.plot(x2, y2 + pi, "--", color="0.5")
+            
+            """
+            # Figure Modifications --------------------------------------------------------
+            # Borders
+            ax.spines["top"].set_color("0.5")
+            ax.spines["bottom"].set_color("0.5")
+            ax.spines["left"].set_color("0.5")
+            ax.spines["right"].set_color("0.5")
+            ax.get_xaxis().set_tick_params(direction="out")
+            ax.get_yaxis().set_tick_params(direction="out")
+            ax.xaxis.tick_bottom()
+            ax.yaxis.tick_left()
+            
+            # Labels
+            plt.title("Fit Plot for Weight", fontsize="14", fontweight="bold")
+            plt.xlabel("Height")
+            plt.ylabel("Weight")
+            plt.xlim(np.min(x) - 1, np.max(x) + 1)
+            
+            # Custom legend
+            handles, labels = ax.get_legend_handles_labels()
+            display = (0, 1)
+            anyArtist = plt.Line2D((0, 1), (0, 0), color="#b9cfe7")    # create custom artists
+            legend = plt.legend(
+                [handle for i, handle in enumerate(handles) if i in display] + [anyArtist],
+                [label for i, label in enumerate(labels) if i in display] + ["95% Confidence Limits"],
+                loc=9, bbox_to_anchor=(0, -0.21, 1., 0.102), ncol=3, mode="expand"
+            )  
+            frame = legend.get_frame().set_edgecolor("0.5")
+            
+            # Save Figure
+            plt.tight_layout()
+            plt.savefig("filename.png", bbox_extra_artists=(legend,), bbox_inches="tight")
+            
+            plt.show()
+            """
+            
+            
+            
+            ax.scatter(exes, whys, color=color, alpha = 0.4)
+            ax.set_title(f'{pt_type}: {col}')
+            ax.set_xlabel('Age (years)')
+            ax.set_ylabel('Volume (cc)')
+            
+            ax.set_xlim(0,50)
+            ax.set_ylim(0,1000)
+       
+    plt.tight_layout()
+    nice_name = os.path.join(out_folder, 'tissue_vol_age_dependency.png')
+    plt.savefig(nice_name)
         
         
